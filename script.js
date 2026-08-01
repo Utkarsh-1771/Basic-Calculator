673b0fbf6b4ba5540d02b3221cefff5b0f4af4c4
Utkarsh Verma <utkarsh1765uv7@gmail.com>
2026-08-02 04:19:27 +0530

    made the buttons interactable

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const screen=document.getElementById("display");
 const buttons=document.querySelectorAll("button");
+let currentInput="0";
+let firstNumber=null;
+let operator=null;
 buttons.forEach(function(button)
 {
     button.addEventListener("click", function()
     {
-        console.log(button.textContent);
+        if(button.textContent==="." && currentInput.includes("."))
+            return;
+        if(currentInput==="0")
+            currentInput=button.textContent;
+        else
+            currentInput+=button.textContent;
+        screen.textContent=currentInput;
+        
     });
 });
