a155b5788bfc703b369f6de0c39fad1e4deec74d
Utkarsh Verma <utkarsh1765uv7@gmail.com>
2026-08-03 01:35:43 +0530

    basic calculations can be done now

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-const screen=document.getElementById("display");
-const buttons=document.querySelectorAll("button");
-let currentInput="0";
-let firstNumber=null;
-let operator=null;
-buttons.forEach(function(button)
-{
-    button.addEventListener("click", function()
-    {
-        if(button.textContent==="." && currentInput.includes("."))
+const screen = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+let currentInput = "0";
+let firstNumber = null;
+let operator = null;
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        if (button.classList.contains("equal-button")) {
+            let result;
+            if (operator == "+")
+                result = firstNumber + Number(currentInput);
+            else if (operator == "−")
+                result = firstNumber - Number(currentInput);
+            else if (operator == "×")
+                result = firstNumber * Number(currentInput);
+            else if (operator == "÷")
+                result = firstNumber / Number(currentInput);
+            currentInput = String(result);
+            screen.textContent = currentInput;
             return;
-        if(currentInput==="0")
-            currentInput=button.textContent;
+        }
+        if (button.classList.contains("operator")) {
+            firstNumber = Number(currentInput);
+            operator = button.textContent;
+            currentInput = "0";
+            return;
+        }
+        if (button.textContent === "." && currentInput.includes("."))
+            return;
+        if (currentInput === "0")
+            currentInput = button.textContent;
         else
-            currentInput+=button.textContent;
-        screen.textContent=currentInput;
-        
+            currentInput += button.textContent;
+        screen.textContent = currentInput;
     });
 });
